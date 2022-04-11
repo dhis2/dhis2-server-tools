9a61f2009b5dfa398d1d3501a6d856acfe784c8b
tkipkurgat <tito@dhis2.org>
2022-04-11 11:19:03 +0300

    Wrote dhis2 role

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,12 @@ sudo snap install lxd
 #sudo apt install ansible # this will not include cummunity modules like ufw, its recommended installing ansible with pip
 # install pip3 
 sudo apt -y install python3-pip
+sudo apt-get install -y python-netaddr #required for ipaddr filter
 
 # install ansible
 # Since pip does not coordinate with system package managers, it could make changes to your system that leaves it in an inconsistent or non-functioning state, its recommended using pip3 with --user
 sudo pip3 install ansible
+
 
 # Ensure a user is a member of lxd group
 # sudo usermod -aG lxd $USER
