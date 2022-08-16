b3e9d5cbe4aff84ef9fc5cb74a0fa5ee68452eb1
tkipkurgat <tito@dhis2.org>
2022-08-16 17:31:17 +0300

    added lxc proxying for port forwarding

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,12 @@
-## bash
-# install lxd on the host with snap
-sudo snap install lxd
-
-# sudo ansible-galaxy collection install community.general
-# this will not include cummunity modules like ufw, its recommended installing ansible with pip
-# install pip3 
-# ------------------------------
-# install ansible with pip
-# ------------------------------
-# Since pip does not coordinate with system package managers, it could make changes to your system that leaves it in an inconsistent or non-functioning state, its recommended using pip3 with --user
-#sudo apt install python3-pip                                # installs  pip3 on Debian like systems.
-#sudo pip3 install ansible                                   # installs ansible with pip3
-
-# ------------------------
+#!/usr/bin/env bash
 # install ansible with apt
-# ------------------------
-# sudo apt update                                             # updates system packages 
-# sudo apt install software-properties-common                 # adds sofware properties common 
-# sudo apt-add-repository --yes --update ppa:ansible/ansible  # adds ansible repository
+sudo apt update                                             # updates system packages 
+sudo apt install software-properties-common                 # adds sofware properties common 
+sudo apt-add-repository --yes --update ppa:ansible/ansible  # adds ansible repository
 sudo apt install ansible                                    # installs ansible
+# ansible dependency 
+sudo apt-get install -y python3-netaddr                      # used for chencking whether a given address is withing a given network
 
-# -----------------------------------------------
-# install community general collections manually
-# -----------------------------------------------
+# install community general collections 
 ansible-galaxy collection install community.general -f      # add community general collections, required if ansible is installed with apt. 
-
-# --------------------------------
-# additional ansible dependencies 
-# --------------------------------
-sudo apt-get install -y python3-netaddr                      #required for ipaddr filter module 
-
-# Ensure a user is a member of lxd group
-# sudo usermod -aG lxd $USER
-
 
