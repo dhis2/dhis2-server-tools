07a3d353fef631541df97c52cc46688f8466dccd
tkipkurgat <tito@dhis2.org>
2023-05-18 09:23:48 +0300

    chore: unattended-upgrades | securing_munin | refactoring & cleanup

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,35 +1,16 @@
 #!/usr/bin/env bash
-# installing ansible 
 set -e 
-if ! command -v ansible &> /dev/null
-then
-  export DEBIAN_FRONTEND=noninteractive
-  RED='\033[0;31m'
-  # disable interactive prompt with  
-  sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf  
-  sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf 
+RED='\033[0;31m'
 
+# Check the version of ubuntu running
+distro_name=$(lsb_release -i | cut -f2)
+distro_version=$(lsb_release -r | cut -f2)
 
-  sudo -E apt -yq update
-  sudo -E apt -yq  upgrade
-  # install git if not present 
-  sudo apt install -yqq  git 
-# install ansible
-  sudo apt install -yqq software-properties-common   
-  sudo apt-add-repository --yes --update ppa:ansible/ansible
-  sudo apt install -yqq  ansible
-# install python3-netaddr required for ansible.utils.ipaddr utility
-  # sudo -E apt-get install -yqq python3-netaddr
-  sudo -E apt-get -qy autoclean
-# install community general collections 
-  ansible-galaxy collection install community.general
-fi
+# UFW status 
+UFW_STATUS=$(sudo ufw status |grep Status|cut -d ' ' -f 2)
 
-if [ $(cat inventory/hosts  | grep -Po '(?<=ansible_connection=)([a-z].*)') == "lxd" ]
-then
-   # ensure firewall is running on the host
-   UFW_STATUS=$(sudo ufw status |grep Status|cut -d ' ' -f 2)
-   if [[ $UFW_STATUS == "inactive" ]]; then
+# ensure firewall is running on the host
+if [[ $UFW_STATUS == "inactive" ]]; then
       echo
 	    echo -e "\e ${RED}=========== ERROR =================="
 	    echo "ufw firewall needs to be enabled in order to perform the installation."
@@ -40,7 +21,51 @@ then
 	    echo "Then you can try to run sudo ./deploy.sh  again"
 	    exit 1
   fi
-   # deploying dhis2 in lxd containers
+# install ansible on ubutnu 20.04 
+ansible_install_2004() {
+  sudo apt -yq update
+  # sudo apt -yq  upgrade
+  sudo apt install -yq  git
+  sudo apt install -yq software-properties-common   
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt install -yq  ansible
+  }
+# install ansible on ubutnu22.04
+  ansible_install_2204() {
+  sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf  
+  sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf  
+  sudo apt -yq update
+  # sudo apt -yq  upgrade
+  sudo apt install -yq  git
+  sudo apt install -yq software-properties-common
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt install -yq ansible
+}
+
+if ! command -v ansible &> /dev/null
+then
+    case ${distro_version} in
+      "20.04")
+        echo "Installing ansible on ${distro_name} ${distro_version} LTS ..."
+        ansible_install_2004
+        ;;
+      "22.04")
+        echo "Installing ansible on ${distro_name} ${distro_version} LTS ..."
+         ansible_install_2204
+        ;;
+      *)
+        echo "Could not install DHIS2 on your Ubuntu version: $distro_version, please upgrade to 20.04|22.04"
+        exit 1
+        ;;
+      esac
+    sudo -E apt-get -yq autoclean
+    # install community general collections 
+    ansible-galaxy collection install community.general
+fi
+
+if [ $(cat inventory/hosts  | grep -Po '(?<=ansible_connection=)([a-z].*)') == "lxd" ]
+then
+    # deploying dhis2 in lxd containers
    echo "Deploying dhis2 with lxd ..."
    sudo ansible-playbook dhis2.yml
 else
